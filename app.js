1cec5f16eb59cfdf27f3e8220ac5d62e66df764f
joann lux <joannlux29@gmail.com>
2022-05-16 10:21:18 +0100

    db-user

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile('./index.html')
+    res.sendFile('./index.html', {root: __dirname})
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+require("dotenv").config();
+
+const mysql = require("mysql");
+let con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
 var pgp = require("pg-promise")(/*options*/);
-var db = pgp("postgres://vignette:azerty@localhost:5432/BaseDD"); //FIXME : exterioriser le port parce-que faille sécurité
+// var db = pgp("postgres://vignette:azerty@localhost:5432/BaseDD"); //FIXME : exterioriser le port parce-que faille sécurité
 
 db.one("SELECT $1 AS value", 123) 
     .then(function (data) {
